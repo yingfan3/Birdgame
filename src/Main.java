9efec30cb574d7f585bb3a64fd3446ef35150430
yingfan <yingfan027@gmail.com>
2023-03-14 13:30:39 -0600

    new

--- src/Main.java
+++ src/Main.java
@@ -23,41 +23,42 @@ public class Main {
         Scanner myObj = new Scanner(System.in);
         System.out.println("Welcome to baobao birdgame!Let's get started");
         while (goal>0){
-            System.out.println("first animal x:");
-            int x1=myObj.nextInt();
-            System.out.println("first animal y:");
-            int y1=myObj.nextInt();
-            board.fristSelected=board.tiles[x1][y1];
-            System.out.println("second animal x:");
-            int x2=myObj.nextInt();
-            System.out.println("second animal y:");
-            int y2=myObj.nextInt();
-            board.secondSelected=board.tiles[x2][y2];
-            board.swap(board.fristSelected, board.secondSelected);
-            if(board.shouldclear()==false){
-                board.swap(board.fristSelected,board.secondSelected);
-            }
-            board.clear();
+            while (board.fristSelected==null||board.secondSelected==null) {
+                System.out.println("x:");
+                int x=myObj.nextInt();
+                System.out.println("y:");
+                int y=myObj.nextInt();
+                board.select(x,y);
 
-            if(board.s.equals(animal)){
-                for (int i = 0; i < board.tiles.length; i++) {
-                    for (int j = 0; j < board.tiles[i].length; j++) {
-                        if(board.tiles[i][j].getSpotAnimal().equals(BoardAnimals.NONE)){
-                            goal--;
+            }
+
+            board.swap(board.fristSelected, board.secondSelected);
+            if(board.shouldclear()==true){
+                board.clear();
+
+                if(board.s.equals(animal)){
+                    for (int i = 0; i < board.tiles.length; i++) {
+                        for (int j = 0; j < board.tiles[i].length; j++) {
+                            if(board.tiles[i][j].getSpotAnimal().equals(BoardAnimals.NONE)){
+                                goal--;
+                            }
                         }
                     }
                 }
-            }
 
 
-            board.fall2();
-            System.out.println("Goal : "+goal+" "+animal+"\n"+board);
+                board.fall2();
+                System.out.println("Goal : "+goal+" "+animal+"\n"+board);
+
+            }else {board.swap(board.fristSelected,board.secondSelected);}
+
 
         }
         System.out.println("Congratualations!You win!");
 
 //        board.fall1();
 //        System.out.println("after fall1 : \n"+board);
+
 
     }
 
